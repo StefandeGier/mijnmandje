decbb0f203d442945fe2646517dc391f85d30c90
StefandeGier <99040150@mydavinci.nl>
2018-04-11 08:55:20 +0200

    changes view

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,28 +1,6 @@
-<!doctype html>
-<html lang="{{ app()->getLocale() }}">
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-
-        <title>Mijnwebshop</title>
-
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
-
-    </head>
-    <body>
+@extends('layouts.app')
+@section('content')
         <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
-                        <a href="{{ route('register') }}">Register</a>
-                    @endauth
-                </div>
-            @endif
 
             <div class="content">
                 <div class="title m-b-md">
@@ -32,5 +10,4 @@
 
             </div>
         </div>
-    </body>
-</html>
+@endsection
