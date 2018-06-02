6874db489cc53cc3fe0faa64edb4c7c0ee1e0833
StefandeGier <99040150@mydavinci.nl>
2018-06-02 17:08:49 +0200

    Commit from Acer

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,7 +6,7 @@
                 <div class="title m-b-md">
                     <h1>Mijnmandje</h1>
 
-                    Commit via mijn git
+                    Commit via Acer laptop
                 </div>
             </div>
         </div>
